4ba979621fca86f2d53c76860a091d89d5068bf6
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-05-30 07:02:47 -0700

    Making Formatting Edits

--- src/main/resources/contruction-co.sql
+++ src/main/resources/contruction-co.sql
@@ -144,94 +144,160 @@ CREATE TABLE IF NOT EXISTS `ConstructionCo`.`Tasks` (
     REFERENCES `ConstructionCo`.`Purchase_Order` (`purchase_order_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+------------------------------------------------------------------------------------------------------------------------
     
 INSERT INTO ConstructionCo.Contractors VALUES (1, 'John Smith', 'johnsmith@gmail.com', '123456789', 'Address 1');
+
 INSERT INTO ConstructionCo.Purchase_Order VALUES (1, 'John Smtih Order', 10000, 'Pending');
+
 INSERT INTO ConstructionCo.Customers VALUES (1, 'Emma Johnson', 'emmajohnson@gmail.com', '987654321', 'Address 1');
+
 INSERT INTO ConstructionCo.Employees VALUES (1, 'John', 'Doe', 'johndoe@gmail.com', '1234567890', '2023-01-01', 'Manager');
+
 INSERT INTO ConstructionCo.Equipment VALUES (1, 'Jack Hammer', 'Description 1', 1);
+
 INSERT INTO ConstructionCo.Invoices VALUES (1, 1, 1, '2023-05-30', 5000);
+
 INSERT INTO ConstructionCo.Materials VALUES (1, 'Ply Wood', 'Description 1', '10.00');
+
 INSERT INTO ConstructionCo.Project_Equipment VALUES (1, 1);
+
 INSERT INTO ConstructionCo.Purchase_Order_Materials VALUES (1, 1);
+
 INSERT INTO ConstructionCo.Suppliers VALUES (1, 'ABC Supplier', 'supplierABC@gmail.com');
+------------------------------------------------------------------------------------------------------------------------
 
 UPDATE ConstructionCo.Contractors SET contractor_name = 'John Johnson' WHERE contractor_id = 1;
+
 UPDATE ConstructionCo.Purchase_Order SET purchaseorder_name = 'John Johnson Order' WHERE purchase_order_id = 1;
+
 UPDATE ConstructionCo.Customers SET customer_name = 'Emma Smith' WHERE customer_id = 1;
+
 UPDATE ConstructionCo.Employees SET first_name = 'Jane' WHERE employee_id = 1;
+
 UPDATE ConstructionCo.Equipment SET equipment_name = 'Drill Machine' WHERE equipment_id = 1;
+
 UPDATE ConstructionCo.Invoices SET total_due = 6000 WHERE invoice_id = 1;
+
 UPDATE ConstructionCo.Materials SET material_name = 'Steel Bars' WHERE material_id = 1;
+
 UPDATE ConstructionCo.Project_Equipment SET equipment_id = 2 WHERE purchase_order_id = 1;
+
 UPDATE ConstructionCo.Purchase_Order_Materials SET material_id = 2 WHERE purchase_order_id = 1;
+
 UPDATE ConstructionCo.Suppliers SET supplier_name = 'XYZ Supplier' WHERE supplier_id = 1;
+------------------------------------------------------------------------------------------------------------------------
 
-DELETE FROM ConstructionCo.Contractors WHERE contractor_id = 1;
-DELETE FROM ConstructionCo.Purchase_Order WHERE purchase_order_id = 1;
-DELETE FROM ConstructionCo.Customers WHERE customer_id = 1;
-DELETE FROM ConstructionCo.Employees WHERE employee_id = 1;
-DELETE FROM ConstructionCo.Equipment WHERE equipment_id = 1;
-DELETE FROM ConstructionCo.Invoices WHERE invoice_id = 1;
-DELETE FROM ConstructionCo.Materials WHERE material_id = 1;
-DELETE FROM ConstructionCo.Project_Equipment WHERE purchase_order_id = 1;
-DELETE FROM ConstructionCo.Purchase_Order_Materials WHERE purchase_order_id = 1;
-DELETE FROM ConstructionCo.Suppliers WHERE supplier_id = 1;
+DELETE FROM ConstructionCo.Contractors
+    WHERE contractor_id = 1;
 
-ALTER TABLE ConstructionCo.Invoices ADD COLUMN payment_status VARCHAR(20) NULL;
-ALTER TABLE ConstructionCo.Materials CHANGE COLUMN price_per_unit price_per_unit DECIMAL(10,2) NULL;
-ALTER TABLE ConstructionCo.Contractors ADD COLUMN rating INT NULL;
-ALTER TABLE ConstructionCo.Customers MODIFY COLUMN phone_number VARCHAR(20);
-ALTER TABLE ConstructionCo.Employees DROP COLUMN hire_date;
+DELETE FROM ConstructionCo.Purchase_Order
+    WHERE purchase_order_id = 1;
 
+DELETE FROM ConstructionCo.Customers
+    WHERE customer_id = 1;
+
+DELETE FROM ConstructionCo.Employees
+    WHERE employee_id = 1;
+
+DELETE FROM ConstructionCo.Equipment
+    WHERE equipment_id = 1;
+
+DELETE FROM ConstructionCo.Invoices
+    WHERE invoice_id = 1;
+
+DELETE FROM ConstructionCo.Materials
+    WHERE material_id = 1;
+
+DELETE FROM ConstructionCo.Project_Equipment
+    WHERE purchase_order_id = 1;
+
+DELETE FROM ConstructionCo.Purchase_Order_Materials
+    WHERE purchase_order_id = 1;
+
+DELETE FROM ConstructionCo.Suppliers
+    WHERE supplier_id = 1;
+------------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE ConstructionCo.Invoices
+    ADD COLUMN payment_status VARCHAR(20) NULL;
+
+ALTER TABLE ConstructionCo.Materials
+    CHANGE COLUMN price_per_unit price_per_unit DECIMAL(10,2) NULL;
+
+ALTER TABLE ConstructionCo.Contractors
+    ADD COLUMN rating INT NULL;
+
+ALTER TABLE ConstructionCo.Customers
+    MODIFY COLUMN phone_number VARCHAR(20);
+
+ALTER TABLE ConstructionCo.Employees
+    DROP COLUMN hire_date;
+------------------------------------------------------------------------------------------------------------------------
     
 SELECT * FROM ConstructionCo.Contractors
-JOIN ConstructionCo.Purchase_Order ON Contractors.contractor_id = Purchase_Order.contractor_id
-JOIN ConstructionCo.Customers ON Customers.customer_id = Purchase_Order.customer_id
-JOIN ConstructionCo.Invoices ON Invoices.invoice_id = Purchase_Order.purchase_order_id
-JOIN ConstructionCo.Employees ON Employees.employee_id = Invoices.contractor_id
-JOIN ConstructionCo.Employee_Tasks ON Employee_Tasks.employee_id = Employees.employee_id
-JOIN ConstructionCo.Tasks ON Tasks.task_id = Employee_Tasks.task_id
-JOIN ConstructionCo.Equipment ON Equipment.equipment_id = Purchase_Order.purchase_order_id
-JOIN ConstructionCo.Project_Equipment ON Project_Equipment.purchase_order_id = Purchase_Order.purchase_order_id
-JOIN ConstructionCo.Materials ON Materials.material_id = Project_Equipment.material_id
-JOIN ConstructionCo.Purchase_Order_Materials ON Purchase_Order_Materials.purchase_order_id = Purchase_Order.purchase_order_id
-JOIN ConstructionCo.Suppliers ON Suppliers.supplier_id = Materials.material_id;    
+    JOIN ConstructionCo.Purchase_Order ON Contractors.contractor_id = Purchase_Order.contractor_id
+    JOIN ConstructionCo.Customers ON Customers.customer_id = Purchase_Order.customer_id
+    JOIN ConstructionCo.Invoices ON Invoices.invoice_id = Purchase_Order.purchase_order_id
+    JOIN ConstructionCo.Employees ON Employees.employee_id = Invoices.contractor_id
+    JOIN ConstructionCo.Employee_Tasks ON Employee_Tasks.employee_id = Employees.employee_id
+    JOIN ConstructionCo.Tasks ON Tasks.task_id = Employee_Tasks.task_id
+    JOIN ConstructionCo.Equipment ON Equipment.equipment_id = Purchase_Order.purchase_order_id
+    JOIN ConstructionCo.Project_Equipment ON Project_Equipment.purchase_order_id = Purchase_Order.purchase_order_id
+    JOIN ConstructionCo.Materials ON Materials.material_id = Project_Equipment.material_id
+    JOIN ConstructionCo.Purchase_Order_Materials ON Purchase_Order_Materials.purchase_order_id = Purchase_Order.purchase_order_id
+    JOIN ConstructionCo.Suppliers ON Suppliers.supplier_id = Materials.material_id;
+------------------------------------------------------------------------------------------------------------------------
 
 SELECT * FROM Customers
-LEFT JOIN Invoices ON Customers.customer_id = Invoices.customer_id;
+    LEFT JOIN Invoices ON Customers.customer_id = Invoices.customer_id;
+
 SELECT *FROM Contractors
-LEFT JOIN Purchase_Order ON Contractors.contractor_id = Purchase_Order.contractor_id;
+    LEFT JOIN Purchase_Order ON Contractors.contractor_id = Purchase_Order.contractor_id;
+
 SELECT * FROM Employees
-LEFT JOIN Employee_Tasks ON Employees.employee_id = Employee_Tasks.employee_id;
+    LEFT JOIN Employee_Tasks ON Employees.employee_id = Employee_Tasks.employee_id;
+
 SELECT * FROM Purchase_Order
-LEFT JOIN Project_Equipment ON Purchase_Order.purchase_order_id = Project_Equipment.purchase_order_id;
+    LEFT JOIN Project_Equipment ON Purchase_Order.purchase_order_id = Project_Equipment.purchase_order_id;
+
 SELECT * FROM Materials
-LEFT JOIN Purchase_Order_Materials ON Materials.material_id = Purchase_Order_Materials.material_id;
+    LEFT JOIN Purchase_Order_Materials ON Materials.material_id = Purchase_Order_Materials.material_id;
+------------------------------------------------------------------------------------------------------------------------
 
 SELECT * FROM Customers
-RIGHT JOIN Invoices ON Customers.customer_id = Invoices.customer_id;
+    RIGHT JOIN Invoices ON Customers.customer_id = Invoices.customer_id;
+
 SELECT * FROM Contractors
-RIGHT JOIN Purchase_Order ON Contractors.contractor_id = Purchase_Order.contractor_id;
+    RIGHT JOIN Purchase_Order ON Contractors.contractor_id = Purchase_Order.contractor_id;
+
 SELECT * FROM Employees
-RIGHT JOIN Employee_Tasks ON Employees.employee_id = Employee_Tasks.employee_id;
+    RIGHT JOIN Employee_Tasks ON Employees.employee_id = Employee_Tasks.employee_id;
+
 SELECT * FROM Purchase_Order
-RIGHT JOIN Project_Equipment ON Purchase_Order.purchase_order_id = Project_Equipment.purchase_order_id;
+    RIGHT JOIN Project_Equipment ON Purchase_Order.purchase_order_id = Project_Equipment.purchase_order_id;
+
 SELECT * FROM Materials
-RIGHT JOIN Purchase_Order_Materials ON Materials.material_id = Purchase_Order_Materials.material_id;
+    RIGHT JOIN Purchase_Order_Materials ON Materials.material_id = Purchase_Order_Materials.material_id;
+
+------------------------------------------------------------------------------------------------------------------------
 
 SELECT contractor_id, Contractors.contractor_name, Purchase_Order.purchaseorder_name FROM Contractors
-INNER JOIN Purchase_Order ON contractor_id = Purchase_Order.contractor_id;
-SELECT customer_id, customer_name, Invoices.due_date FROM Customers
-INNER JOIN Invoices ON customer_id = Invoices.customer_id;
-SELECT employee_id, first_name, Employee_Tasks.due_date FROM Employees
-INNER JOIN Employee_Tasks ON Employees.employee_id = Employee_Tasks.employee_id;
-SELECT Purchase_Order.purchase_order_id, Project_Equipment.equipment_id FROM Purchase_Order
-INNER JOIN Project_Equipment ON Purchase_Order.purchase_order_id = Project_Equipment.purchase_order_id;
-SELECT material_id, purchase_order_id FROM Materials
-INNER JOIN Purchase_Order_Materials ON Materials.material_id = Purchase_Order_Materials.material_id;
+    INNER JOIN Purchase_Order ON contractor_id = Purchase_Order.contractor_id;
 
-----------------------------------------------------------------------------------------------------------
+SELECT customer_id, customer_name, Invoices.due_date FROM Customers
+    INNER JOIN Invoices ON customer_id = Invoices.customer_id;
+
+SELECT employee_id, first_name, Employee_Tasks.due_date FROM Employees
+    INNER JOIN Employee_Tasks ON Employees.employee_id = Employee_Tasks.employee_id;
+
+SELECT Purchase_Order.purchase_order_id, Project_Equipment.equipment_id FROM Purchase_Order
+    INNER JOIN Project_Equipment ON Purchase_Order.purchase_order_id = Project_Equipment.purchase_order_id;
+
+SELECT material_id, purchase_order_id FROM Materials
+    INNER JOIN Purchase_Order_Materials ON Materials.material_id = Purchase_Order_Materials.material_id;
+
+------------------------------------------------------------------------------------------------------------------------
 SELECT customer_id, Invoices.invoice_id FROM Customers
     LEFT JOIN Invoices ON Customers.customer_id = Invoices.customer_id;
 
