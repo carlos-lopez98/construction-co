bee2fe33b72000a26af74a60d34b0ee72d2cc308
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:00:56 -0700

    Employee model implementation, fields, getters and setters

--- src/main/java/com/solvd/constructionco/models/Employee.java
+++ src/main/java/com/solvd/constructionco/models/Employee.java
@@ -1,4 +1,67 @@
 package com.solvd.constructionco.models;
 
+import java.time.LocalDate;
+
 public class Employee {
+
+    private int employeeId;
+    private String employeeName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate hireDate;
+    private String position;
+
+    public Employee(String employeeName, String email, String position){
+        this.employeeName = employeeName;
+        this.email = email;
+        this.position = position;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
