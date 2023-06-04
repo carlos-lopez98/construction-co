0816291e7706ea152b5d688f3c04bf33da81eb88
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 11:00:20 -0700

    Updating ConstructionDAO access modifiers

--- src/main/java/com/solvd/constructionco/interfaces/ContructionDAO.java
+++ src/main/java/com/solvd/constructionco/interfaces/ContructionDAO.java
@@ -6,9 +6,9 @@ import java.util.List;
 /*Provides a generic DAO*/
 public interface ContructionDAO<T, ID> {
 
-    T getById(ID id);
-    void save(T entity);
-    void update(T entity);
-    void delete(ID id);
-    List<T> getAll();
+    public T getById(ID id);
+    public void save(T entity);
+    public void update(T entity);
+    public void delete(ID id);
+    public List<T> getAll();
 }
