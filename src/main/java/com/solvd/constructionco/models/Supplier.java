8b9b3053673c42fb34c290eb7966f294ea28148a
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:23:14 -0700

    Supplier Model, fields getters and setters

--- src/main/java/com/solvd/constructionco/models/Supplier.java
+++ src/main/java/com/solvd/constructionco/models/Supplier.java
@@ -1,4 +1,40 @@
 package com.solvd.constructionco.models;
 
 public class Supplier {
+    private int supplierId;
+    private String supplierName;
+    private String email;
+
+    public Supplier(){
+
+    };
+
+    public Supplier(String supplierName, String email){
+      this.supplierName = supplierName;
+      this.email = email;
+    };
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
