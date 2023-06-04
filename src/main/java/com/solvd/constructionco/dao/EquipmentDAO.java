8f1b01a43e422be87eb27e26f0db0e48cc9aa3f9
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:40:40 -0700

    EquipmentDAO imps ConstructionDAO

--- src/main/java/com/solvd/constructionco/dao/EquipmentDAO.java
+++ src/main/java/com/solvd/constructionco/dao/EquipmentDAO.java
@@ -1,4 +1,33 @@
 package com.solvd.constructionco.dao;
 
-public class EquipmentDAO {
+import com.solvd.constructionco.interfaces.ConstructionDAO;
+
+import java.util.List;
+
+public class EquipmentDAO implements ConstructionDAO {
+
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public void save(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
 }
