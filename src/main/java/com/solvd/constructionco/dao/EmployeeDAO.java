981164a51fc4bbe6a22dae2458839db07aa78346
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:40:09 -0700

    CustomerDAO impls ConstructionDAO

--- src/main/java/com/solvd/constructionco/dao/EmployeeDAO.java
+++ src/main/java/com/solvd/constructionco/dao/EmployeeDAO.java
@@ -1,4 +1,32 @@
 package com.solvd.constructionco.dao;
 
-public class EmployeeDAO {
+import com.solvd.constructionco.interfaces.ConstructionDAO;
+
+import java.util.List;
+
+public class EmployeeDAO implements ConstructionDAO {
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public void save(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
 }
