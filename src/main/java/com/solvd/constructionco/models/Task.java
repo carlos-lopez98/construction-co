fab67ff14934d2fc57bf141eaba63518636a6517
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:24:49 -0700

    Task Model, fields, setters and getters

--- src/main/java/com/solvd/constructionco/models/Task.java
+++ src/main/java/com/solvd/constructionco/models/Task.java
@@ -1,4 +1,51 @@
 package com.solvd.constructionco.models;
 
+import java.time.LocalDate;
+
 public class Task {
+
+    private int taskId;
+    private String taskName;
+    private boolean isClosed;
+    private LocalDate dueDate;
+
+    public Task(){
+
+    };
+
+    public Task(String taskName){
+
+    };
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
 }
