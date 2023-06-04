374b039b8a1f1723290cf9562d1934dc427b6b39
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:04:06 -0700

    Equipment Model Implementation, fields, getters and setters

--- src/main/java/com/solvd/constructionco/models/Equipment.java
+++ src/main/java/com/solvd/constructionco/models/Equipment.java
@@ -1,4 +1,43 @@
 package com.solvd.constructionco.models;
 
 public class Equipment {
+    private int equipmentId;
+    private String equipmentName;
+    private String description;
+    private boolean available;
+
+    public Equipment(){
+    };
+
+    public int getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(int equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public void setEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
