16afeec368b00aa7b95e315ce9953381f12fff6b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:41:40 -0700

    MaterialDAO implements ConstructionDAO

--- src/main/java/com/solvd/constructionco/dao/MaterialDAO.java
+++ src/main/java/com/solvd/constructionco/dao/MaterialDAO.java
@@ -1,4 +1,32 @@
 package com.solvd.constructionco.dao;
 
-public class MaterialDAO {
+import com.solvd.constructionco.interfaces.ConstructionDAO;
+
+import java.util.List;
+
+public class MaterialDAO implements ConstructionDAO {
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public void save(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
 }
