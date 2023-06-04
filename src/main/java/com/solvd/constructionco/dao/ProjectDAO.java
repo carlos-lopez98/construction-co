6e7021545f81c2254dbc4d7cd4d41848e606a884
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:42:04 -0700

    ProjectDAO imps ConstructionDAO

--- src/main/java/com/solvd/constructionco/dao/ProjectDAO.java
+++ src/main/java/com/solvd/constructionco/dao/ProjectDAO.java
@@ -1,4 +1,32 @@
 package com.solvd.constructionco.dao;
 
-public class ProjectDAO {
+import com.solvd.constructionco.interfaces.ConstructionDAO;
+
+import java.util.List;
+
+public class ProjectDAO implements ConstructionDAO {
+    @Override
+    public Object getById(Object o) {
+        return null;
+    }
+
+    @Override
+    public void save(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
 }
