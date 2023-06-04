bd1733ad0893853c314c1931d4b48001e76b4a3d
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 12:21:36 -0700

    Project Model, fields, getters and setters'

--- src/main/java/com/solvd/constructionco/models/Project.java
+++ src/main/java/com/solvd/constructionco/models/Project.java
@@ -1,4 +1,45 @@
 package com.solvd.constructionco.models;
 
 public class Project {
+
+    private int purchaseOrderId;
+    private String purchaseOrderName;
+    private int budget;
+    private boolean isClosed;
+
+    public Project(){
+
+    };
+
+    public int getPurchaseOrderId() {
+        return purchaseOrderId;
+    }
+
+    public void setPurchaseOrderId(int purchaseOrderId) {
+        this.purchaseOrderId = purchaseOrderId;
+    }
+
+    public String getPurchaseOrderName() {
+        return purchaseOrderName;
+    }
+
+    public void setPurchaseOrderName(String purchaseOrderName) {
+        this.purchaseOrderName = purchaseOrderName;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
 }
