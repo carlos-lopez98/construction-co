74199b571c795fb8ceb59f4d0b897501ad176326
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-04 11:57:21 -0700

    Customer Model Implementation, fields, getters and setters

--- src/main/java/com/solvd/constructionco/models/Customer.java
+++ src/main/java/com/solvd/constructionco/models/Customer.java
@@ -1,4 +1,55 @@
 package com.solvd.constructionco.models;
 
 public class Customer {
+
+    private int customerId;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public Customer(String customerName, String phoneNumber){
+        this.customerName = customerName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
