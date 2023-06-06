414e7f0e9696e6a97bf5ec55c143d2e0aadee7d2
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:20:51 -0700

    Overriding methods in CustomerDAO

--- src/main/java/com/solvd/constructionco/dao/CustomerDAO.java
+++ src/main/java/com/solvd/constructionco/dao/CustomerDAO.java
@@ -1,34 +1,57 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CustomerDAO implements ConstructionDAO {
+public class CustomerDAO implements ConstructionDAO<Customer, Integer> {
 
+    private List<Customer> customers;
+
+    public CustomerDAO() {
+        customers = new ArrayList<>();
+    }
 
     @Override
-    public Object getById(Object o) {
+    public Customer getById(Integer id) {
+
+        for (Customer customer : customers) {
+            if (customer.getCustomerId() == id) {
+                return customer;
+            }
+        }
+
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Customer customer) {
+        customers.add(customer);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Customer customer) {
+        Customer existingCustomer = getById(customer.getCustomerId());
+        if (existingCustomer != null) {
+            existingCustomer.setCustomerName(customer.getCustomerName());
+            existingCustomer.setEmail(customer.getEmail());
+            existingCustomer.setPhoneNumber(customer.getPhoneNumber());
+            existingCustomer.setAddress(customer.getAddress());
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        Customer customerToRemove = getById(id);
+        if (customerToRemove != null) {
+            customers.remove(customerToRemove);
+        }
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Customer> getAll() {
+        return customers;
     }
 }
--- src/main/java/com/solvd/constructionco/service/services/TaskService.java
+++ src/main/java/com/solvd/constructionco/service/services/TaskService.java
@@ -1,32 +1,42 @@
 package com.solvd.constructionco.service.services;
 
+import com.solvd.constructionco.dao.TaskDAO;
 import com.solvd.constructionco.interfaces.ConstructionServiceOperations;
+import com.solvd.constructionco.models.Task;
 
 import java.util.List;
 
-public class TaskService implements ConstructionServiceOperations {
+public class TaskService implements ConstructionServiceOperations<Task, Integer> {
+
+    private TaskDAO taskDAO;
+
+    public TaskService(TaskDAO taskDAO){
+        this.taskDAO = taskDAO;
+    }
+
+
     @Override
-    public Object getById(Object o) {
+    public Task getById(Integer integer) {
         return null;
     }
 
     @Override
-    public void save(Object entity) {
+    public void save(Task entity) {
 
     }
 
     @Override
-    public void update(Object entity) {
+    public void update(Task entity) {
 
     }
 
     @Override
-    public void delete(Object o) {
+    public void delete(Integer integer) {
 
     }
 
     @Override
-    public List getAll() {
+    public List<Task> getAll() {
         return null;
     }
 }
