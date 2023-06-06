d105c4db42d315ead74050743c12d85d0c19f2ab
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:32:05 -0700

    Overriding methods in MaterialDAO

--- src/main/java/com/solvd/constructionco/dao/MaterialDAO.java
+++ src/main/java/com/solvd/constructionco/dao/MaterialDAO.java
@@ -1,32 +1,56 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Material;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MaterialDAO implements ConstructionDAO {
+public class MaterialDAO implements ConstructionDAO<Material, Integer> {
+
+    private List<Material> materials;
+
+    public MaterialDAO() {
+        materials = new ArrayList<>();
+    }
+
     @Override
-    public Object getById(Object o) {
+    public Material getById(Integer id) {
+        for (Material material : materials) {
+            if (material.getMaterialId() == id) {
+                return material;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Material material) {
+        materials.add(material);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Material material) {
+        Material existingMaterial = getById(material.getMaterialId());
+        if (existingMaterial != null) {
+            existingMaterial.setSupplierId(material.getSupplierId());
+            existingMaterial.setMaterialName(material.getMaterialName());
+            existingMaterial.setDescription(material.getDescription());
+            existingMaterial.setPricePerUnit(material.getPricePerUnit());
+            existingMaterial.setUnit(material.getUnit());
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        Material materialToRemove = getById(id);
+        if (materialToRemove != null) {
+            materials.remove(materialToRemove);
+        }
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Material> getAll() {
+        return materials;
     }
 }
