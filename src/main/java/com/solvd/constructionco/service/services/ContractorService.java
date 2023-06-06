3b57b3e13718319c309e59b2245a49b5a4477b00
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:43:14 -0700

    Overriding methods in ContractorService

--- src/main/java/com/solvd/constructionco/service/services/ContractorService.java
+++ src/main/java/com/solvd/constructionco/service/services/ContractorService.java
@@ -1,32 +1,41 @@
 package com.solvd.constructionco.service.services;
 
+import com.solvd.constructionco.dao.ContractorDAO;
 import com.solvd.constructionco.interfaces.ConstructionServiceOperations;
+import com.solvd.constructionco.models.Contractor;
 
 import java.util.List;
 
-public class ContractorService implements ConstructionServiceOperations {
-    @Override
-    public Object getById(Object o) {
-        return null;
+public class ContractorService implements ConstructionServiceOperations<Contractor, Integer> {
+
+    private ContractorDAO contractorDAO;
+
+    public ContractorService(ContractorDAO contractorDAO) {
+        this.contractorDAO = contractorDAO;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public Contractor getById(Integer id) {
+        return contractorDAO.getById(id);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void save(Contractor entity) {
+        contractorDAO.save(entity);
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void update(Contractor entity) {
+        contractorDAO.update(entity);
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public void delete(Integer id) {
+        contractorDAO.delete(id);
+    }
+
+    @Override
+    public List<Contractor> getAll() {
+        return contractorDAO.getAll();
     }
 }
