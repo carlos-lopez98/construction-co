5f47fae3b2ee7bde1d88aa015829d0721df6c9bf
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:36:19 -0700

    Overriding methods in ProjectDAO

--- src/main/java/com/solvd/constructionco/dao/ProjectDAO.java
+++ src/main/java/com/solvd/constructionco/dao/ProjectDAO.java
@@ -1,32 +1,52 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Project;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectDAO implements ConstructionDAO {
+public class ProjectDAO implements ConstructionDAO<Project, Integer> {
+    private List<Project> projects;
+
+    public ProjectDAO() {
+        projects = new ArrayList<>();
+    }
+
     @Override
-    public Object getById(Object o) {
+    public Project getById(Integer id) {
+        for (Project project : projects) {
+            if (project.getPurchaseOrderId() == id) {
+                return project;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Project project) {
+        projects.add(project);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Project project) {
+        int projectId = project.getPurchaseOrderId();
+        for (int i = 0; i < projects.size(); i++) {
+            Project existingProject = projects.get(i);
+            if (existingProject.getPurchaseOrderId() == projectId) {
+                projects.set(i, project);
+                return;
+            }
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        projects.removeIf(project -> project.getPurchaseOrderId() == id);
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Project> getAll() {
+        return new ArrayList<>(projects);
     }
 }
