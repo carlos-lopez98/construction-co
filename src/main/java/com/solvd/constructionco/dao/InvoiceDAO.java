3b90eff7849593c8a18ee6f3dc7be830025555ae
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:29:42 -0700

    Overriding methods in InvoiceDAO

--- src/main/java/com/solvd/constructionco/dao/InvoiceDAO.java
+++ src/main/java/com/solvd/constructionco/dao/InvoiceDAO.java
@@ -1,32 +1,56 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Invoice;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class InvoiceDAO implements ConstructionDAO {
+public class InvoiceDAO implements ConstructionDAO<Invoice, Integer> {
+
+    private List<Invoice> invoices;
+
+    public InvoiceDAO() {
+        invoices = new ArrayList<>();
+    }
+
     @Override
-    public Object getById(Object o) {
+    public Invoice getById(Integer id) {
+        for (Invoice invoice : invoices) {
+            if (invoice.getInvoiceId() == id) {
+                return invoice;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Invoice invoice) {
+        invoices.add(invoice);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Invoice invoice) {
+        Invoice existingInvoice = getById(invoice.getInvoiceId());
+        if (existingInvoice != null) {
+            existingInvoice.setPurchaseOrderId(invoice.getPurchaseOrderId());
+            existingInvoice.setCustomerId(invoice.getCustomerId());
+            existingInvoice.setContractorId(invoice.getContractorId());
+            existingInvoice.setDueDate(invoice.getDueDate());
+            existingInvoice.setTotalDue(invoice.getTotalDue());
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        Invoice invoiceToRemove = getById(id);
+        if (invoiceToRemove != null) {
+            invoices.remove(invoiceToRemove);
+        }
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Invoice> getAll() {
+        return invoices;
     }
 }
