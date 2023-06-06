36a067bf362be7941a3e510e38e3be4f7aecb0a3
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:37:45 -0700

    Overriding methods in SupplierDAO

--- src/main/java/com/solvd/constructionco/dao/SupplierDAO.java
+++ src/main/java/com/solvd/constructionco/dao/SupplierDAO.java
@@ -1,32 +1,52 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Supplier;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SupplierDAO implements ConstructionDAO {
+public class SupplierDAO implements ConstructionDAO<Supplier, Integer> {
+    private List<Supplier> suppliers;
+
+    public SupplierDAO() {
+        suppliers = new ArrayList<>();
+    }
+
     @Override
-    public Object getById(Object o) {
+    public Supplier getById(Integer id) {
+        for (Supplier supplier : suppliers) {
+            if (supplier.getSupplierId() == id) {
+                return supplier;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Supplier supplier) {
+        suppliers.add(supplier);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Supplier supplier) {
+        int supplierId = supplier.getSupplierId();
+        for (int i = 0; i < suppliers.size(); i++) {
+            Supplier existingSupplier = suppliers.get(i);
+            if (existingSupplier.getSupplierId() == supplierId) {
+                suppliers.set(i, supplier);
+                return;
+            }
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        suppliers.removeIf(supplier -> supplier.getSupplierId() == id);
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Supplier> getAll() {
+        return new ArrayList<>(suppliers);
     }
 }
