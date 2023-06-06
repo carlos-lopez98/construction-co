ed0ef2a8ba1abe568531e50de82919dfc8546b1a
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:47:11 -0700

    Overriding methods in EmployeeService

--- src/main/java/com/solvd/constructionco/service/services/EmployeeService.java
+++ src/main/java/com/solvd/constructionco/service/services/EmployeeService.java
@@ -1,32 +1,43 @@
 package com.solvd.constructionco.service.services;
 
+import com.solvd.constructionco.dao.EmployeeDAO;
 import com.solvd.constructionco.interfaces.ConstructionServiceOperations;
+import com.solvd.constructionco.models.Employee;
 
 import java.util.List;
 
-public class EmployeeService implements ConstructionServiceOperations {
+public class EmployeeService implements ConstructionServiceOperations<Employee, Integer> {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeService(EmployeeDAO employeeDAO){
+
+        this.employeeDAO = employeeDAO;
+    }
+
+
     @Override
-    public Object getById(Object o) {
-        return null;
+    public Employee getById(Integer employeeID) {
+        return employeeDAO.getById(employeeID);
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Employee employee) {
+            employeeDAO.save(employee);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Employee employee) {
+        employeeDAO.update(employee);
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer employeeID) {
+        employeeDAO.delete(employeeID);
     }
 
     @Override
     public List getAll() {
-        return null;
+        return employeeDAO.getAll();
     }
 }
