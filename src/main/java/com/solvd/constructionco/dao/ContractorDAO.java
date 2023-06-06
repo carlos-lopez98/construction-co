8f42853c8b21436393fe766d9fdb6129061c8b0f
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:24:58 -0700

    Overriding methods in ContractorDAO

--- src/main/java/com/solvd/constructionco/dao/ContractorDAO.java
+++ src/main/java/com/solvd/constructionco/dao/ContractorDAO.java
@@ -1,34 +1,64 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Contractor;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ContractorDAO implements ConstructionDAO {
+public class ContractorDAO implements ConstructionDAO<Contractor, Integer> {
 
+
+    private List<Contractor> contractors;
+
+    public ContractorDAO() {
+        contractors = new ArrayList<>();
+    }
 
     @Override
-    public Object getById(Object o) {
+    public Contractor getById(Integer id) {
+        for (Contractor contractor : contractors) {
+            if (contractor.getContractorId() == id) {
+                return contractor;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Contractor contractor) {
+        contractors.add(contractor);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Contractor contractor) {
+        Contractor existingContractor = getById(contractor.getContractorId());
+        if (existingContractor != null) {
+            existingContractor.setContractorName(contractor.getContractorName());
+            existingContractor.setEmail(contractor.getEmail());
+            existingContractor.setPhoneNumber(contractor.getPhoneNumber());
+            existingContractor.setAddress(contractor.getAddress());
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        Contractor contractorToRemove = getById(id);
+        if (contractorToRemove != null) {
+            contractors.remove(contractorToRemove);
+        }
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Contractor> getAll() {
+        return contractors;
     }
 }
+
+
+
+
+
+
+
+
