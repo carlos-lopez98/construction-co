d3ff70420a838af91843c6dab04845dad5adbdf5
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:26:29 -0700

    Overriding methods in EquipmentDAO

--- src/main/java/com/solvd/constructionco/dao/EquipmentDAO.java
+++ src/main/java/com/solvd/constructionco/dao/EquipmentDAO.java
@@ -1,33 +1,61 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Equipment;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class EquipmentDAO implements ConstructionDAO {
+public class EquipmentDAO implements ConstructionDAO<Equipment, Integer> {
+
+    private List<Equipment> equipmentList;
+
+    public EquipmentDAO() {
+        equipmentList = new ArrayList<>();
+    }
 
     @Override
-    public Object getById(Object o) {
+    public Equipment getById(Integer id) {
+        for (Equipment equipment : equipmentList) {
+            if (equipment.getEquipmentId() == id) {
+                return equipment;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Equipment equipment) {
+        equipmentList.add(equipment);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Equipment equipment) {
+        Equipment existingEquipment = getById(equipment.getEquipmentId());
+        if (existingEquipment != null) {
+            existingEquipment.setEquipmentName(equipment.getEquipmentName());
+            existingEquipment.setDescription(equipment.getDescription());
+            existingEquipment.setAvailable(equipment.isAvailable());
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        Equipment equipmentToRemove = getById(id);
+        if (equipmentToRemove != null) {
+            equipmentList.remove(equipmentToRemove);
+        }
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Equipment> getAll() {
+        return equipmentList;
     }
 }
+
+
+
+
+
+
+
