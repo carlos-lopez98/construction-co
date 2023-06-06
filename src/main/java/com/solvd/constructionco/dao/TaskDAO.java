d9309803e082d722f6d06ac3a9903c8d6d536375
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-06 06:39:19 -0700

    Overriding methods in TaskDAO

--- src/main/java/com/solvd/constructionco/dao/TaskDAO.java
+++ src/main/java/com/solvd/constructionco/dao/TaskDAO.java
@@ -1,33 +1,52 @@
 package com.solvd.constructionco.dao;
 
 import com.solvd.constructionco.interfaces.ConstructionDAO;
+import com.solvd.constructionco.models.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TaskDAO implements ConstructionDAO {
+public class TaskDAO implements ConstructionDAO<Task, Integer> {
+    private List<Task> tasks;
+
+    public TaskDAO() {
+        tasks = new ArrayList<>();
+    }
 
     @Override
-    public Object getById(Object o) {
+    public Task getById(Integer id) {
+        for (Task task : tasks) {
+            if (task.getTaskId() == id) {
+                return task;
+            }
+        }
         return null;
     }
 
     @Override
-    public void save(Object entity) {
-
+    public void save(Task task) {
+        tasks.add(task);
     }
 
     @Override
-    public void update(Object entity) {
-
+    public void update(Task task) {
+        int taskId = task.getTaskId();
+        for (int i = 0; i < tasks.size(); i++) {
+            Task existingTask = tasks.get(i);
+            if (existingTask.getTaskId() == taskId) {
+                tasks.set(i, task);
+                return;
+            }
+        }
     }
 
     @Override
-    public void delete(Object o) {
-
+    public void delete(Integer id) {
+        tasks.removeIf(task -> task.getTaskId() == id);
     }
 
     @Override
-    public List getAll() {
-        return null;
+    public List<Task> getAll() {
+        return new ArrayList<>(tasks);
     }
 }
