44a270189203690e3835fde31bc1c7da15890d92
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-07 08:04:33 -0700

    iTaskDAO implemented

--- src/main/java/com/solvd/constructionco/dao/iTaskDAO.java
+++ src/main/java/com/solvd/constructionco/dao/iTaskDAO.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.dao;
 
-public interface iTaskDAO {
+import com.solvd.constructionco.models.Task;
+
+import java.util.List;
+
+public interface iTaskDAO extends ConstructionDAO<Task, Integer>{
+
+    @Override
+    Task getById(Integer taskID);
+
+    @Override
+    void save(Task task);
+
+    @Override
+    void update(Task task);
+
+    @Override
+    void delete(Integer taskID);
+
+    @Override
+    List<Task> getAll();
 }
