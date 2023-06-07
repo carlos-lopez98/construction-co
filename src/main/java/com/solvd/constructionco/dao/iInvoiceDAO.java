09106403f527d61d1cca6c00eaa05cc2e9b6e25e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-07 08:00:55 -0700

    iInvoiceDAO implemented

--- src/main/java/com/solvd/constructionco/dao/iInvoiceDAO.java
+++ src/main/java/com/solvd/constructionco/dao/iInvoiceDAO.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.dao;
 
-public interface iInvoiceDAO {
+import com.solvd.constructionco.models.Invoice;
+
+import java.util.List;
+
+public interface iInvoiceDAO extends ConstructionDAO<Invoice, Integer>{
+
+    @Override
+    Invoice getById(Integer invoiceID);
+
+    @Override
+    void save(Invoice invoice);
+
+    @Override
+    void update(Invoice invoice);
+
+    @Override
+    void delete(Integer invoiceID);
+
+    @Override
+    List<Invoice> getAll();
 }
