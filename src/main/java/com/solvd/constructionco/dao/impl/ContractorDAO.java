5c7a45895231df4a560840b7df59cd743f116151
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-08 07:14:35 -0700

    Added SQL Queries to ContractorDAO

--- src/main/java/com/solvd/constructionco/dao/impl/ContractorDAO.java
+++ src/main/java/com/solvd/constructionco/dao/impl/ContractorDAO.java
@@ -1,19 +1,26 @@
 package com.solvd.constructionco.dao.impl;
 
-import com.solvd.constructionco.dao.ConstructionDAO;
+import com.solvd.constructionco.Main;
 import com.solvd.constructionco.dao.iContractorDAO;
 import com.solvd.constructionco.models.Contractor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContractorDAO implements iContractorDAO<Contractor, Integer> {
 
-
+    private static final Logger logger = LogManager.getLogger(Main.class);
     private List<Contractor> contractors;
 
 
-    private static final String getAllQuery = "SELECT contractor_id, contractor_name, email, phone_number, address FROM contractors";
+    private final String getAllQuery = "SELECT contractor_id, contractor_name, email, phone_number, address FROM contractors";
+    private final String getByIdQuery = "SELECT contractor_id, contractor_name, email, phone_number, address FROM contractors WHERE contractor_id = ?";
+    private final String saveQuery = "INSERT INTO contractors VALUES(?,?,?,?,?)";
+    private final String deleteQuery = "DELETE FROM contractors WHERE contractor_id = ?";
+    private final String updateQuery = "UPDATE contractors SET contractor_name = ?, email = ?, phone_number = ?, address = ? WHERE contractor_id = ?";
+
 
     public ContractorDAO() {
         contractors = new ArrayList<>();
