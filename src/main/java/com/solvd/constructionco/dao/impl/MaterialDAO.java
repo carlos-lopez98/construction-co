2845508a01f433936aad601d76097e4b87741753
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-08 21:29:49 -0700

    Added SQL Queries to MaterialDAO

--- src/main/java/com/solvd/constructionco/dao/impl/MaterialDAO.java
+++ src/main/java/com/solvd/constructionco/dao/impl/MaterialDAO.java
@@ -11,6 +11,13 @@ public class MaterialDAO implements iMaterialDAO<Material, Integer> {
 
     private List<Material> materials;
 
+    private final String getAllQuery = "SELECT material_id, supplier_id, material_name, description, price_per_unit FROM materials";
+    private final String getByIdQuery = "SELECT material_id, supplier_id, material_name, description, price_per_unit FROM materials WHERE material_id = ?";
+    private final String saveQuery = "INSERT INTO materials VALUES(?,?,?,?,?)";
+    private final String deleteQuery = "DELETE FROM materials WHERE material_id = ?";
+    private final String updateQuery = "UPDATE materials SET material_id = ?, supplier_id = ?, material_name = ?, description = ?, price_per_unit = ? WHERE material_id = ?";
+
+
     public MaterialDAO() {
         materials = new ArrayList<>();
     }
