e7e8fcdb3bfe9add660c4a10c547bfe86aa20a98
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 20:30:34 -0700

    Refactored/Added ITaskService

--- src/main/java/com/solvd/constructionco/service/ITaskService.java
+++ src/main/java/com/solvd/constructionco/service/ITaskService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface ITaskService {
+import com.solvd.constructionco.models.Task;
+
+import java.util.List;
+
+public interface ITaskService extends ConstructionServiceOperations<Task, Integer>{
+
+    @Override
+    <T> T getById(Integer taskID);
+
+    @Override
+    void save(Task task);
+
+    @Override
+    void update(Task task);
+
+    @Override
+    void delete(Integer taskID);
+
+    @Override
+    List<Task> getAll();
 }
