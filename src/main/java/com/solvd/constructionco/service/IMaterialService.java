162a73fb418c5662f2a78099c075d40a7955840e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 20:28:27 -0700

    Refactored/Added IMaterialService

--- src/main/java/com/solvd/constructionco/service/IMaterialService.java
+++ src/main/java/com/solvd/constructionco/service/IMaterialService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface IMaterialService {
+import com.solvd.constructionco.models.Material;
+
+import java.util.List;
+
+public interface IMaterialService extends ConstructionServiceOperations<Material, Integer>{
+
+    @Override
+    <T> T getById(Integer materialID);
+
+    @Override
+    void save(Material material);
+
+    @Override
+    void update(Material material);
+
+    @Override
+    void delete(Integer materialID);
+
+    @Override
+    List<Material> getAll();
 }
