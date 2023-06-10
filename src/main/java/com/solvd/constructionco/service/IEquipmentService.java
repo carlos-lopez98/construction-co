92f79f50113ca5a2bdbbb9dcf30df3f3af5f34df
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 20:26:57 -0700

    Refactored/Added IEquipmentService

--- src/main/java/com/solvd/constructionco/service/IEquipmentService.java
+++ src/main/java/com/solvd/constructionco/service/IEquipmentService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface IEquipmentService {
+import com.solvd.constructionco.models.Equipment;
+
+import java.util.List;
+
+public interface IEquipmentService extends ConstructionServiceOperations<Equipment, Integer>{
+
+    @Override
+    <T> T getById(Integer equipmentID);
+
+    @Override
+    void save(Equipment equipment);
+
+    @Override
+    void update(Equipment equipment);
+
+    @Override
+    void delete(Integer equipmentID);
+
+    @Override
+    List<Equipment> getAll();
 }
