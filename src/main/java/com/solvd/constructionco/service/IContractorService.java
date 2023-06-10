41af45683daa67015461faca323f0225daeab81b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 20:25:04 -0700

    Refactored ICustomerService

--- src/main/java/com/solvd/constructionco/service/IContractorService.java
+++ src/main/java/com/solvd/constructionco/service/IContractorService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface IContractorService extends ConstructionServiceOperations{
+import com.solvd.constructionco.models.Contractor;
+
+import java.util.List;
+
+public interface IContractorService extends ConstructionServiceOperations<Contractor, Integer>{
+
+    @Override
+    <T> T getById(Integer contractorID);
+
+    @Override
+    void save(Contractor contractor);
+
+    @Override
+    void update(Contractor contractor);
+
+    @Override
+    void delete(Integer contractorID);
+
+    @Override
+    List<Contractor> getAll();
 }
--- src/main/java/com/solvd/constructionco/service/ICustomerService.java
+++ src/main/java/com/solvd/constructionco/service/ICustomerService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface ICustomerService {
+import com.solvd.constructionco.models.Customer;
+
+import java.util.List;
+
+public interface ICustomerService extends ConstructionServiceOperations<Customer, Integer>{
+
+    @Override
+    <T> T getById(Integer customerID);
+
+    @Override
+    void save(Customer customer);
+
+    @Override
+    void update(Customer customer);
+
+    @Override
+    void delete(Integer customerID);
+
+    @Override
+    List<Customer> getAll();
 }
