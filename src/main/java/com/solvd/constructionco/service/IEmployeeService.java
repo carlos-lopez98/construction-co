9fecfeeaecb712f531e86278fc03a3625aeeee76
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 20:26:08 -0700

    Refactored/Added IEmployeeService

--- src/main/java/com/solvd/constructionco/service/IEmployeeService.java
+++ src/main/java/com/solvd/constructionco/service/IEmployeeService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface IEmployeeService {
+import com.solvd.constructionco.models.Employee;
+
+import java.util.List;
+
+public interface IEmployeeService extends ConstructionServiceOperations<Employee, Integer>{
+
+    @Override
+    <T> T getById(Integer employeeID);
+
+    @Override
+    void save(Employee employee);
+
+    @Override
+    void update(Employee employee);
+
+    @Override
+    void delete(Integer employeeID);
+
+    @Override
+    List<Employee> getAll();
 }
