641e172d6e6519e9722736230e2143f1af9f4b2a
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 20:29:52 -0700

    Refactored/Added ISupplierService

--- src/main/java/com/solvd/constructionco/service/ISupplierService.java
+++ src/main/java/com/solvd/constructionco/service/ISupplierService.java
@@ -1,4 +1,23 @@
 package com.solvd.constructionco.service;
 
-public interface ISupplierService {
+import com.solvd.constructionco.models.Supplier;
+
+import java.util.List;
+
+public interface ISupplierService extends ConstructionServiceOperations<Supplier, Integer>{
+
+    @Override
+    <T> T getById(Integer supplierID);
+
+    @Override
+    void save(Supplier supplier);
+
+    @Override
+    void update(Supplier supplier);
+
+    @Override
+    void delete(Integer supplierID);
+
+    @Override
+    List<Supplier> getAll();
 }
