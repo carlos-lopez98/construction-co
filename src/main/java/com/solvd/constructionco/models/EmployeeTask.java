72efc5f6e229d4d32311fcecbd9eef7127536689
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 22:18:27 -0700

    EmployeeTask implemented

--- src/main/java/com/solvd/constructionco/models/EmployeeTask.java
+++ src/main/java/com/solvd/constructionco/models/EmployeeTask.java
@@ -1,6 +1,45 @@
 package com.solvd.constructionco.models;
 
+import java.sql.Date;
+
 public class EmployeeTask {
 
+    private int taskID;
+    private int employeeID;
+    private Date dueDate;
+    private boolean status;
 
+    public EmployeeTask(){};
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
