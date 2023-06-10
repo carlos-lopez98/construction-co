b71aabb6de7453fc5cfefe566b4218826522dbc8
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-09 23:04:02 -0700

    Fixed DOMContractorParser

--- src/main/java/com/solvd/constructionco/util/parse/impl/DOMContractorParser.java
+++ src/main/java/com/solvd/constructionco/util/parse/impl/DOMContractorParser.java
@@ -1,17 +1,24 @@
 package com.solvd.constructionco.util.parse.impl;
 
+import com.solvd.constructionco.Main;
 import com.solvd.constructionco.models.Contractor;
 import com.solvd.constructionco.util.parse.iParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 
 public class DOMContractorParser implements iParser<Contractor> {
 
+        private static final Logger logger = LogManager.getLogger(Main.class);
         private Document document;
 
         public DOMContractorParser() {
@@ -31,8 +38,8 @@ public class DOMContractorParser implements iParser<Contractor> {
                  DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
                  DocumentBuilder builder = factory.newDocumentBuilder();
                  document = builder.parse(new File(filePath));
-             } catch (Exception e) {
-                 e.printStackTrace();
+             } catch (IOException | SAXException | ParserConfigurationException e) {
+                 logger.info("Exception " + e.getClass().getSimpleName() + " was thrown, Details: " + e);
              }
 
          Contractor contractor = new Contractor();
