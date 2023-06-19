f7e0d23c4a8799c051b1cae2e71b61da93bc7b9b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-19 16:14:22 -0700

    Implementng interfaces for ProjectService

--- src/main/java/com/solvd/constructionco/service/mybatisimpl/ProjectService.java
+++ src/main/java/com/solvd/constructionco/service/mybatisimpl/ProjectService.java
@@ -1,4 +1,33 @@
 package com.solvd.constructionco.service.mybatisimpl;
 
-public class ProjectService {
+import com.solvd.constructionco.models.Project;
+import com.solvd.constructionco.service.interfaces.IProjectService;
+
+import java.util.List;
+
+public class ProjectService implements IProjectService {
+    @Override
+    public <T> T getById(Integer projectID) {
+        return null;
+    }
+
+    @Override
+    public void save(Project project) {
+
+    }
+
+    @Override
+    public void update(Project project) {
+
+    }
+
+    @Override
+    public void delete(Integer projectID) {
+
+    }
+
+    @Override
+    public List<Project> getAll() {
+        return null;
+    }
 }
