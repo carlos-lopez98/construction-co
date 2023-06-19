5c3e323d2fe3704897e286dd3594ce0e98aa57cc
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-19 16:13:58 -0700

    Implementng interfaces for mybatis Services

--- src/main/java/com/solvd/constructionco/service/mybatisimpl/CustomerService.java
+++ src/main/java/com/solvd/constructionco/service/mybatisimpl/CustomerService.java
@@ -1,4 +1,35 @@
 package com.solvd.constructionco.service.mybatisimpl;
 
-public class CustomerService {
+import com.solvd.constructionco.models.Customer;
+import com.solvd.constructionco.service.interfaces.ICustomerService;
+
+import java.util.List;
+
+public class CustomerService implements ICustomerService {
+
+
+    @Override
+    public <T> T getById(Integer customerID) {
+        return null;
+    }
+
+    @Override
+    public void save(Customer customer) {
+
+    }
+
+    @Override
+    public void update(Customer customer) {
+
+    }
+
+    @Override
+    public void delete(Integer customerID) {
+
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return null;
+    }
 }
--- src/main/java/com/solvd/constructionco/service/mybatisimpl/InvoiceService.java
+++ src/main/java/com/solvd/constructionco/service/mybatisimpl/InvoiceService.java
@@ -1,4 +1,33 @@
 package com.solvd.constructionco.service.mybatisimpl;
 
-public class InvoiceService {
+import com.solvd.constructionco.models.Invoice;
+import com.solvd.constructionco.service.interfaces.IInvoiceService;
+
+import java.util.List;
+
+public class InvoiceService implements IInvoiceService {
+    @Override
+    public <T> T getById(Integer invoiceID) {
+        return null;
+    }
+
+    @Override
+    public void save(Invoice invoice) {
+
+    }
+
+    @Override
+    public void update(Invoice invoice) {
+
+    }
+
+    @Override
+    public void delete(Integer invoiceID) {
+
+    }
+
+    @Override
+    public List<Invoice> getAll() {
+        return null;
+    }
 }
