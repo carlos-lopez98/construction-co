ae029147fcb48fa1308ac3e5a21374f15e797d93
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-20 06:26:00 -0700

    Added an invoice service mybatis implementation

--- src/main/java/com/solvd/constructionco/service/mybatisimpl/InvoiceService.java
+++ src/main/java/com/solvd/constructionco/service/mybatisimpl/InvoiceService.java
@@ -1,12 +1,23 @@
 package com.solvd.constructionco.service.mybatisimpl;
 
+import com.solvd.constructionco.Main;
 import com.solvd.constructionco.models.Invoice;
 import com.solvd.constructionco.service.interfaces.IInvoiceService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class InvoiceService implements IInvoiceService {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    private static final String BATIS_CONFIG = "src/main/resources/config/mybatis-config.xml";
+    private static final String SAVE_PROJECT = "com.solvd.constructionco.mybatis.impl.invoicemapper.save";
+    private static final String GET_BY_ID = "com.solvd.constructionco.mybatis.impl.invoicemapper.getById";
+    private static final String UPDATE_PROJECT = "com.solvd.constructionco.mybatis.impl.invoicemapper.update";
+    private static final String DELETE_PROJECT = "com.solvd.constructionco.mybatis.impl.invoicemapper.delete";
+    private static final String GET_ALL = "com.solvd.constructionco.mybatis.impl.invoicemapper.getAll";
 
     @Override
     public Invoice getById(Integer invoiceId) {
