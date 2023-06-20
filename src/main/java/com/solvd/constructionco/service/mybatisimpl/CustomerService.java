8675aa3b4ce71ab2880c2d6b1cd588103bc62bed
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-06-19 17:35:47 -0700

    CustomerService implemented using MyBatis

--- src/main/java/com/solvd/constructionco/service/mybatisimpl/CustomerService.java
+++ src/main/java/com/solvd/constructionco/service/mybatisimpl/CustomerService.java
@@ -20,23 +20,39 @@ import java.util.Properties;
 public class CustomerService implements ICustomerService {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
-    private static final String BATISCONFIG = "src/main/resources/config/mybatis-config.xml";
-    private static final String SAVECUSTOMER = "com.solvd.constructionco.mybatis.impl.customermapper.save";
-
-
+    private static final String BATIS_CONFIG = "src/main/resources/config/mybatis-config.xml";
+    private static final String SAVE_CUSTOMER = "com.solvd.constructionco.mybatis.impl.customermapper.save";
+    private static final String GET_BY_ID = "com.solvd.constructionco.mybatis.impl.customermapper.getById";
+    private static final String UPDATE_CUSTOMER = "com.solvd.constructionco.mybatis.impl.customermapper.update";
+    private static final String DELETE_CUSTOMER = "com.solvd.constructionco.mybatis.impl.customermapper.delete";
+    private static final String GET_ALL = "com.solvd.constructionco.mybatis.impl.customermapper.getAll";
 
     @Override
-    public <T> T getById(Integer customerID) {
-        return null;
+    public Customer getById(Integer customerId) {
+
+            Properties prop = this.retrieveProperties();
+            Customer customer = null;
+
+            try (InputStream stream = Resources.getResourceAsStream(BATIS_CONFIG);
+                 SqlSession session = new SqlSessionFactoryBuilder().build(stream, prop).openSession();) {
+
+                customer = session.selectOne(GET_BY_ID, customerId);
+
+                return customer;
+            } catch (IOException e) {
+                logger.info("File Not Found");
+            }
+
+        return customer;
     }
 
     @Override
     public void save(Customer customer) {
         Properties properties = this.retrieveProperties();
 
-        try (InputStream stream = Resources.getResourceAsStream(BATISCONFIG);
+        try (InputStream stream = Resources.getResourceAsStream(BATIS_CONFIG);
              SqlSession session = new SqlSessionFactoryBuilder().build(stream, properties).openSession();) {
-            session.selectOne(SAVECUSTOMER, customer);
+            session.selectOne(SAVE_CUSTOMER, customer);
             session.commit();
         } catch (IOException e) {
             logger.info("File Not Found");
@@ -47,16 +63,57 @@ public class CustomerService implements ICustomerService {
     @Override
     public void update(Customer customer) {
 
+        Properties properties = this.retrieveProperties();
+
+        try (InputStream stream = Resources.getResourceAsStream(BATIS_CONFIG);
+             SqlSession session = new SqlSessionFactoryBuilder().build(stream, properties).openSession();) {
+            session.selectOne(UPDATE_CUSTOMER, customer);
+            session.commit();
+
+            logger.info("Succesfully updated Customer " + customer.getCustomerName());
+        } catch (IOException e) {
+            logger.info("File Not Found");
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
-    public void delete(Integer customerID) {
+    public void delete(Integer customerId) {
+
+        Properties properties = this.retrieveProperties();
+
+        try (InputStream stream = Resources.getResourceAsStream(BATIS_CONFIG);
+             SqlSession session = new SqlSessionFactoryBuilder().build(stream, properties).openSession();) {
+            session.selectOne(DELETE_CUSTOMER, customerId);
+            session.commit();
+
+            logger.info("Succesfully deleted customer with ID:" + customerId);
+        } catch (IOException e) {
+            logger.info("File Not Found");
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public List<Customer> getAll() {
-        return null;
+
+        Properties properties = this.retrieveProperties();
+        List<Customer> customers = null;
+
+        try (InputStream stream = Resources.getResourceAsStream(BATIS_CONFIG);
+             SqlSession session = new SqlSessionFactoryBuilder().build(stream, properties).openSession();) {
+            customers = session.selectList(GET_ALL);
+            session.commit();
+
+            logger.info("Succesfully retrieved all customers in database");
+        } catch (IOException e) {
+            logger.info("File Not Found");
+            throw new RuntimeException(e);
+        }
+
+        return customers;
     }
 
 
